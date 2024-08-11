344a0c18ac9e44f7a697ed398410160031e9629d
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-08-11 17:48:02 +0300

    test

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html lang="ar">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
