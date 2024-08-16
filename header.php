d620eba6ac15d8a997be927519bc9ae981eb3b2a
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-08-16 15:38:22 +0300

    fix header on scroll

--- header.php
+++ header.php
@@ -73,7 +73,7 @@ $current_language = apply_filters('wpml_current_language', NULL);
                                 <div class='toggle-text-on'>EN</div>
                             </div>
                         </li> -->
-                        <li>
+                        <li style="margin-bottom: 0; height: fit-content;">
                             <svg class="cusrsor-pointer" data-bs-toggle="modal" data-bs-target="#search" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 50 50">
                                 <path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"></path>
                             </svg>
@@ -84,7 +84,7 @@ $current_language = apply_filters('wpml_current_language', NULL);
                                     $save_for_mobile_logo = $menu_item['general_image'];
                         ?>
                             <li id="logo-main">
-                                <a href="<?php echo $menu_item['url']; ?>">
+                                <a style="height: fit-content; display: none;" href="<?php echo $menu_item['url']; ?>">
                                     <img class="logo" src="<?php echo $menu_item['general_image']; ?>" alt="truthtrend">
                                 </a>
                             </li>
@@ -210,6 +210,15 @@ $current_language = apply_filters('wpml_current_language', NULL);
         //     $('.industries-dropdown .menu_item_main').toggleClass('active_page');
         //     $('.menu_on_mobile').toggleClass('scroll_in_mobile_menu');
         // })
+        $(window).scroll(function() {
+            var currentScreenPosition = $(document).scrollTop();
+            if (currentScreenPosition > 250) {
+                $(".header-container").addClass("active");
+            } else {
+                $(".header-container").removeClass("active");
+
+            }
+        });
     });
 </script>
 <div class="site-content">
