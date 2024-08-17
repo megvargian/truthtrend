71487ac89f73eb4de1ff4a5beaa335bcf99ac956
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-08-17 16:53:50 +0300

    test

--- blocks/Homepage_Blocks/block_1.php
+++ blocks/Homepage_Blocks/block_1.php
@@ -34,8 +34,12 @@ if($customFields_main_post['youtube_url']){
                         <?php }else{?>
                             <a class="position-relative" href="<?php echo get_permalink($block1_fields['main_post'] -> ID) ?>">
                                 <div class="position-relative cusotm-header-position">
-                                    <h2><?php echo $block1_fields['main_post'] -> post_title; ?></h2>
-                                    <img class="w-100 add-border-radius d-none d-sm-block image-darker" src="<?php echo $customFields_main_post['images']['tablet_image'] ?>" alt="">
+                                    <div class="middle-section">
+                                        <h2 class="pb-3"><?php echo $block1_fields['main_post'] -> post_title; ?></h2>
+                                        <a href="<?php echo get_permalink($post_custom_fields['author'] -> ID); ?>">
+                                            <?php echo get_the_title($post_custom_fields['author'] -> ID); ?>
+                                        </a>
+                                    </div>                                    <img class="w-100 add-border-radius d-none d-sm-block image-darker" src="<?php echo $customFields_main_post['images']['tablet_image'] ?>" alt="">
                                     <img class="w-100 add-border-radius d-block d-sm-none image-darker" src="<?php echo $customFields_main_post['images']['mobile_image'] ?>" alt="">
                                 </div>
                             </a>
@@ -45,8 +49,12 @@ if($customFields_main_post['youtube_url']){
                         <?php if($isYoutube){ ?>
                             <a class="position-relative" href="<?php echo $customFields_main_post['youtube_url']; ?>">
                                 <div class="position-relative cusotm-header-position">
-                                    <h2><?php echo $block1_fields['main_post'] -> post_title; ?></h2>
-                                    <img class="w-100 add-border-radius d-none d-sm-block image-darker" src="<?php echo $customFields_main_post['images']['tablet_image'] ?>" alt="">
+                                    <div class="middle-section">
+                                        <h2 class="pb-3"><?php echo $block1_fields['main_post'] -> post_title; ?></h2>
+                                        <a href="<?php echo get_permalink($post_custom_fields['author'] -> ID); ?>">
+                                            <?php echo get_the_title($post_custom_fields['author'] -> ID); ?>
+                                        </a>
+                                    </div>                                    <img class="w-100 add-border-radius d-none d-sm-block image-darker" src="<?php echo $customFields_main_post['images']['tablet_image'] ?>" alt="">
                                     <img class="w-100 add-border-radius d-block d-sm-none image-darker" src="<?php echo $customFields_main_post['images']['mobile_image'] ?>" alt="">
                                     <img class="play" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/play-48.ico" alt="play">
                                 </div>
@@ -54,8 +62,12 @@ if($customFields_main_post['youtube_url']){
                         <?php }else{?>
                             <a class="position-relative" href="<?php echo get_permalink($block1_fields['main_post'] -> ID) ?>">
                                 <div class="position-relative cusotm-header-position">
-                                    <h2><?php echo $block1_fields['main_post'] -> post_title; ?></h2>
-                                    <img class="w-100 add-border-radius d-none d-sm-block image-darker" src="<?php echo $customFields_main_post['images']['tablet_image'] ?>" alt="">
+                                    <div class="middle-section">
+                                        <h2 class="pb-3"><?php echo $block1_fields['main_post'] -> post_title; ?></h2>
+                                        <a href="<?php echo get_permalink($post_custom_fields['author'] -> ID); ?>">
+                                            <?php echo get_the_title($post_custom_fields['author'] -> ID); ?>
+                                        </a>
+                                    </div>                                    <img class="w-100 add-border-radius d-none d-sm-block image-darker" src="<?php echo $customFields_main_post['images']['tablet_image'] ?>" alt="">
                                     <img class="w-100 add-border-radius d-block d-sm-none image-darker" src="<?php echo $customFields_main_post['images']['mobile_image'] ?>" alt="">
                                 </div>
                             </a>
