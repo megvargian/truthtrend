f52986991dc192fcf49852a7d69bb2890566a4c1
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-08-18 20:21:36 +0300

    test

--- blocks/Homepage_Blocks/block_15.php
+++ blocks/Homepage_Blocks/block_15.php
@@ -12,11 +12,31 @@ $args = array(
     'order' => 'DESC',
 );
 $most_viewed_query = new WP_Query($args);
+$second_args = array(
+    'post_type'      => 'post',         // Specify post type, 'post' for blog posts
+    'posts_per_page' => 4,             // Number of posts to display
+    'orderby'        => 'date',         // Order by date
+    'order'          => 'DESC',         // Newest first
+    'category__in'   => [32, 33, 34],      // Array of category IDs (replace with your category IDs)
+);
+$query = new WP_Query($second_args);
+
+if ($query->have_posts()) {
+    while ($query->have_posts()) {
+        $query->the_post();
+        // Your loop code here, such as displaying the post title and date
+        the_title('<h2>', '</h2>');
+        echo '<p>' . get_the_date() . '</p>';
+    }
+    wp_reset_postdata();
+} else {
+    echo 'No posts found';
+}
 ?>
 <section>
     <div class="container">
         <div class="row">
-            <div class="col-md-6 col-12 text-right">
+            <div class="col-md-4 col-12 text-right">
                 <h2 class="text-black mb-md-5 mb-2 border-right-custom">
                     <?php echo $block15_fields['most_readbles']['title'] ?>
                 </h2>
@@ -25,11 +45,10 @@ $most_viewed_query = new WP_Query($args);
                         // Loop through the posts
                         while ($most_viewed_query->have_posts()) {
                             $most_viewed_query->the_post();
-
                             $get_custom_feilds = get_fields(get_the_ID());
                     ?>
                         <li>
-                            <a class="mt-2 text-black a-h3" href="<?php echo get_permalink(get_the_ID()); ?>">
+                            <a class="mt-2 text-black a-h3" style="font-size: 2rem;" href="<?php echo get_permalink(get_the_ID()); ?>">
                                 <?php echo get_the_title(get_the_ID());?>
                             </a>
                             <a class="mt-2" href="<?php echo get_permalink($get_custom_feilds['author'] -> ID); ?>" style="font-size: 1rem; line-height: 4">
