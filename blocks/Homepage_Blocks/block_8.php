52cb9948fca681cefc3bd3503186f110e324dc69
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-08-18 14:04:44 +0300

    fix

--- blocks/Homepage_Blocks/block_8.php
+++ blocks/Homepage_Blocks/block_8.php
@@ -11,7 +11,7 @@ $all_posts = array_merge($swiper_posts, $posts);
     <div class="container">
         <div class="row pb-3 mb-2 pb-lg-5 mb-lg-4">
             <div class="col-12">
-                <a href="<?= $block8_fields['title_url']; ?>">
+                <a href="<?php echo $block8_fields['title_url']; ?>">
                     <h2 class="text-center"><?php echo $block8_fields['title']; ?></h2>
                 </a>
             </div>
@@ -19,7 +19,7 @@ $all_posts = array_merge($swiper_posts, $posts);
         <div class="row gx-5 d-none d-lg-flex">
             <div class="col-4">
                 <div class="row text-right pb-3">
-                    <a href="<?= $block8_fields['sub_title_url']; ?>">
+                    <a href="<?php echo $block8_fields['sub_title_url']; ?>">
                         <h4><?php echo $block8_fields['sub_title']; ?></h4>
                     </a>
                 </div>
@@ -28,10 +28,10 @@ $all_posts = array_merge($swiper_posts, $posts);
                       $post_custom_fields = get_fields($post['post'] -> ID);
                     ?>
                         <div class="col-12 mb-4 horizontal-saperator-right-side d-flex">
-                            <img class="h-auto" style="width: 100px;" src="<?php echo $post_custom_fields['images']['desktop_image']; ?>" alt="<?php echo $post['post'] -> post_title ?>">
+                            <img class="h-auto" style="width: 100px;" src="<?php echo $post_custom_fields['images']['desktop_image']; ?>" alt="<?php echo $post['post'] -> post_title; ?>">
                             <div style="padding-right: 10px;">
                                 <a href="<?php echo get_permalink($post['post'] -> ID); ?>">
-                                    <h5 class="mt-2"><?php echo $post['post'] -> post_title ?></h5>
+                                    <h5 class="mt-2"><?php echo $post['post'] -> post_title; ?></h5>
                                 </a>
                                 <a class="mt-2" href="<?php echo get_permalink($post_custom_fields['author'] -> ID); ?>" style="font-size: 1rem;">
                                     <?php echo get_the_title($post_custom_fields['author'] -> ID); ?>
@@ -49,9 +49,9 @@ $all_posts = array_merge($swiper_posts, $posts);
                             $post_custom_fields = get_fields($post['post'] -> ID);
                         ?>
                                 <div class="swiper-slide">
-                                    <img class="w-100 custom-height-second-index" src="<?php echo $post_custom_fields['images']['desktop_image']; ?>" alt="<?php echo $post['post'] -> post_title ?>">
+                                    <img class="w-100 custom-height-second-index" src="<?php echo $post_custom_fields['images']['desktop_image']; ?>" alt="<?php echo $post['post'] -> post_title; ?>">
                                     <a href="<?php echo get_permalink($post['post'] -> ID); ?>">
-                                        <h2 class="mt-3"><?php echo $post['post'] -> post_title ?></h2>
+                                        <h2 class="mt-3"><?php echo $post['post'] -> post_title; ?></h2>
                                     </a>
                                     <p><?php echo $post['post'] -> post_excerpt; ?></p>
                                     <a class="mt-4" href="<?php echo get_permalink($post_custom_fields['author'] -> ID); ?>">
@@ -67,7 +67,7 @@ $all_posts = array_merge($swiper_posts, $posts);
             <div class="swiper ThirdSwiperMobile">
                 <div class="swiper-wrapper">
                     <?php foreach($all_posts as $post){
-                        $get_field = get_fields($post['post'] -> ID)
+                        $get_field = get_fields($post['post'] -> ID);
                     ?>
                         <div class="swiper-slide">
                             <div>
@@ -76,7 +76,7 @@ $all_posts = array_merge($swiper_posts, $posts);
                                     <img class="w-100 h-100 d-block d-sm-none" src="<?php echo $get_field['images']['mobile_image']; ?>" alt="<?php echo $post['post'] -> post_title; ?>">
                                     <h5 class="mb-3"><?php echo $post['post'] -> post_title; ?></h5>
                                 </a>
-                                <a href="<?php echo get_permalink($get_field['author'] -> ID) ;?>">
+                                <a href="<?php echo get_permalink($get_field['author'] -> ID); ?>">
                                     <?php echo get_the_title($get_field['author'] -> ID); ?>
                                 </a>
                             </div>
