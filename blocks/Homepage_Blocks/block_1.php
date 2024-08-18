0872569983af2c1052310b1ee2a9df4fde9c6db8
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-08-18 11:55:15 +0300

    fix block 1

--- blocks/Homepage_Blocks/block_1.php
+++ blocks/Homepage_Blocks/block_1.php
@@ -10,18 +10,13 @@ $block1_fields = get_fields();
             <div class="swiper firstSwiperDesktop">
                 <div class="swiper-wrapper">
                     <?php
-                    foreach ($block1_fields as $main_post) {
-                        $customFields_main_post = get_fields($main_post['main_post'] -> ID);
+                    foreach ($block1_fields['main_posts'] as $main_post) {
                         $post_custom_fields = get_fields($main_post['main_post'] -> ID);
-                        if($customFields_main_post['youtube_url']){
-                            $isYoutube = true;
-                        } else{
-                            $isYoutube = false;
-                        }
+                        $isYoutube = $post_custom_fields['youtube_url'] ? true: false;
                     ?>
                         <div class="swiper-slide">
                             <?php if($isYoutube){ ?>
-                                <a class="position-relative" href="<?php echo $customFields_main_post['youtube_url']; ?>">
+                                <a class="position-relative" href="<?php echo $post_custom_fields['youtube_url']; ?>">
                                     <div class="position-relative cusotm-header-position">
                                         <div class="middle-section">
                                             <h2 class="pb-3"><?php echo $main_post['main_post'] -> post_title; ?></h2>
@@ -29,8 +24,8 @@ $block1_fields = get_fields();
                                                 <?php echo get_the_title($post_custom_fields['author'] -> ID); ?>
                                             </a>
                                         </div>
-                                        <img class="w-100 add-border-radius d-none d-sm-block image-darker" src="<?php echo $customFields_main_post['images']['tablet_image'] ?>" alt="">
-                                        <img class="w-100 add-border-radius d-block d-sm-none image-darker" src="<?php echo $customFields_main_post['images']['mobile_image'] ?>" alt="">
+                                        <img class="w-100 add-border-radius d-none d-sm-block image-darker" src="<?php echo $post_custom_fields['images']['tablet_image'] ?>" alt="">
+                                        <img class="w-100 add-border-radius d-block d-sm-none image-darker" src="<?php echo $post_custom_fields['images']['mobile_image'] ?>" alt="">
                                         <img class="play" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/play-48.ico" alt="play">
                                     </div>
                                 </a>
@@ -43,8 +38,8 @@ $block1_fields = get_fields();
                                                 <?php echo get_the_title($post_custom_fields['author'] -> ID); ?>
                                             </a>
                                         </div>
-                                        <img class="w-100 add-border-radius d-none d-sm-block image-darker" src="<?php echo $customFields_main_post['images']['tablet_image'] ?>" alt="<?php echo $main_post['main_post'] -> post_title; ?>">
-                                        <img class="w-100 add-border-radius d-block d-sm-none image-darker" src="<?php echo $customFields_main_post['images']['mobile_image'] ?>" alt="<?php echo $main_post['main_post'] -> post_title; ?>">
+                                        <img class="w-100 add-border-radius d-none d-sm-block image-darker" src="<?php echo $post_custom_fields['images']['tablet_image'] ?>" alt="<?php echo $main_post['main_post'] -> post_title; ?>">
+                                        <img class="w-100 add-border-radius d-block d-sm-none image-darker" src="<?php echo $post_custom_fields['images']['mobile_image'] ?>" alt="<?php echo $main_post['main_post'] -> post_title; ?>">
                                     </div>
                                 </a>
                             <?php } ?>
