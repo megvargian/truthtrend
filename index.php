28ac1ceadcd879acb6edb67d2e84b7150e2be505
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-08-18 13:37:24 +0300

    add breakpoints

--- index.php
+++ index.php
@@ -38,6 +38,23 @@ get_header();
                 type: 'bullets',
                 clickable: true,
             },
+            breakpoints: {
+                // when window width is >= 320px
+                320: {
+                slidesPerView: 1,
+                spaceBetween: 10
+                },
+                // when window width is >= 480px
+                480: {
+                slidesPerView: 2,
+                spaceBetween: 20
+                },
+                // when window width is >= 640px
+                640: {
+                slidesPerView: 4,
+                spaceBetween: 20
+                }
+            }
         });
         var swiper = new Swiper(".firstSwiperDesktop", {
             slidesPerView: 1,
