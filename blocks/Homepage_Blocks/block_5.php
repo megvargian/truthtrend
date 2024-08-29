a57aed0c0f336c135f308b029891bba06985a314
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-08-29 15:03:06 +0300

    fix block 5

--- blocks/Homepage_Blocks/block_5.php
+++ blocks/Homepage_Blocks/block_5.php
@@ -5,33 +5,33 @@
 $block5_fields = get_fields();
 $post_custom_fields = get_fields($block5_fields['post'] -> ID);
 ?>
-<section class="pb-0">
+<section class="pb-0 block-5">
     <div class="container">
         <div class="row text-center pb-3 mb-2 pb-lg-5 mb-lg-4">
             <h2>ملف الأسبوع</h2>
         </div>
     </div>
     <div class="container-fluid gx-0">
-        <div class="row gx-0">
+        <!-- <div class="row gx-0">
             <div class="col-md-6 col-12">
-                <img class="w-100 h-100 d-none d-lg-block " src="<?php echo $post_custom_fields['images']['desktop_image']; ?>">
-                <img class="w-100 h-100 d-none d-sm-block d-lg-none" src="<?php echo $post_custom_fields['images']['tablet_image']; ?>" alt="">
-                <img class="w-100 h-100 d-block d-sm-none" src="<?php echo $post_custom_fields['images']['mobile_image']; ?>" alt="">
+                <img class="w-100 h-100 d-none d-lg-block " src="<?php //echo $post_custom_fields['images']['desktop_image']; ?>">
+                <img class="w-100 h-100 d-none d-sm-block d-lg-none" src="<?php //echo $post_custom_fields['images']['tablet_image']; ?>" alt="">
+                <img class="w-100 h-100 d-block d-sm-none" src="<?php //echo $post_custom_fields['images']['mobile_image']; ?>" alt="">
             </div>
             <div class="col-md-6 col-12 bg-black">
                 <div class="p-5">
-                    <a href="<?php echo get_permalink( $block5_fields['post'] -> ID ); ?>">
-                        <h3><?php echo $block5_fields['post'] -> post_title; ?></h3>
+                    <a href="<?php //echo get_permalink( $block5_fields['post'] -> ID ); ?>">
+                        <h3><?php //echo $block5_fields['post'] -> post_title; ?></h3>
                     </a>
                     <p class="text-white" style="max-width: 30rem;">
-                    <?php echo $block5_fields['post'] -> post_excerpt; ?>
+                    <?php //echo $block5_fields['post'] -> post_excerpt; ?>
                     </p>
-                    <a class="text-white" href="<?php echo get_permalink($post_custom_fields['author'] -> ID); ?>">
-                        <?php echo get_the_title($post_custom_fields['author'] -> ID); ?>
+                    <a class="text-white" href="<?php //echo get_permalink($post_custom_fields['author'] -> ID); ?>">
+                        <?php //echo get_the_title($post_custom_fields['author'] -> ID); ?>
                     </a>
                 </div>
             </div>
-        </div>
+        </div> -->
         <div class="row gx-0">
             <div class="position-relative cusotm-header-position">
                 <div class="middle-section">
